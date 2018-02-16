629206114ba383d59b3ad6e931fa606ad606c77e
Connor Carey <connorbcarey@hotmail.com>
2018-02-15 19:05:02 -0700

    closes #109

--- server/src/main/java/com/tripco/t08/planner/DistanceCalc.java
+++ server/src/main/java/com/tripco/t08/planner/DistanceCalc.java
@@ -14,11 +14,15 @@ public class DistanceCalc {
             return -1;
         }
         else {
-            double x = Math.cos(dest.getLatitude()) * Math.cos(dest.getLongitude()) - Math.cos(source.getLatitude()) * Math.cos(source.getLongitude());//cos(Theta2)*cos(Lamda2)- cos(Theta1)*cos(Lamda1)
-            double y = Math.cos(dest.getLatitude()) * Math.sin(dest.getLongitude()) - Math.cos(source.getLatitude()) * Math.sin(source.getLongitude());//cos(Theta2)*sin(Lamda2)- cos(Theta1)*sin(Lamda1)
-            double z = Math.sin(dest.getLatitude()) - Math.sin(source.getLatitude());//sin(Theta2)-sin(Theta1)
-            double c = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));//sqrt(x^2+y^2+z^2)
-            double rho = 2 * Math.asin(c / 2);//2arcsin(C/2)
+            double destLat = degreesToRad(dest.getLatitude());
+            double destLong = degreesToRad(dest.getLongitude());
+            double sourceLat = degreesToRad(source.getLatitude());
+            double sourceLong = degreesToRad(source.getLongitude());
+            double x = Math.cos(destLat)*Math.cos(destLong)-Math.cos(sourceLat)*Math.cos(sourceLong);//cos(Theta2)*cos(Lamda2)- cos(Theta1)*cos(Lamda1)
+            double y = Math.cos(destLat)*Math.sin(destLong)-Math.cos(sourceLat)*Math.sin(sourceLong);//cos(Theta2)*sin(Lamda2)- cos(Theta1)*sin(Lamda1)
+            double z = Math.sin(destLat)-Math.sin(sourceLat);//sin(Theta2)-sin(Theta1)
+            double c = Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));//sqrt(x^2+y^2+z^2)
+            double rho = 2*Math.asin(c/2);//2arcsin(C/2)
             double d = Math.round(unit.getConversionFactor() * rho);//radius*central angle (in miles km would be 6371.0088)
             return (int)d;
         }
