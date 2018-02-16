49ec46f633f7e98da265894bbef73de567eee46d
Connor Carey <connorbcarey@hotmail.com>
2018-02-15 18:56:40 -0700

    closes #120

---
+++ server/src/main/java/com/tripco/t08/planner/DistanceCalc.java
@@ -0,0 +1,36 @@
+package com.tripco.t08.planner;
+
+public class DistanceCalc {
+
+    /**
+     *
+     * @param source Coordinate of the originating place
+     * @param dest Coordinate of the destination place
+     * @param unit Distance unit in mile or kilometer in a double
+     * @return
+     */
+    public double distanceTo(Coordinate source, Coordinate dest, DistanceUnit unit) {
+        if (source == null || dest == null) {
+            return -1;
+        }
+        else {
+            double x = Math.cos(dest.getLatitude()) * Math.cos(dest.getLongitude()) - Math.cos(source.getLatitude()) * Math.cos(source.getLongitude());//cos(Theta2)*cos(Lamda2)- cos(Theta1)*cos(Lamda1)
+            double y = Math.cos(dest.getLatitude()) * Math.sin(dest.getLongitude()) - Math.cos(source.getLatitude()) * Math.sin(source.getLongitude());//cos(Theta2)*sin(Lamda2)- cos(Theta1)*sin(Lamda1)
+            double z = Math.sin(dest.getLatitude()) - Math.sin(source.getLatitude());//sin(Theta2)-sin(Theta1)
+            double c = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));//sqrt(x^2+y^2+z^2)
+            double rho = 2 * Math.asin(c / 2);//2arcsin(C/2)
+            double d = Math.round(unit.getConversionFactor() * rho);//radius*central angle (in miles km would be 6371.0088)
+            return (int)d;
+        }
+    }
+
+
+    /**
+     * Converts a number from degrees to radians
+     * @param degree the double in degrees
+     * @return
+     */
+    public double degreesToRad(double degree){//convert float input in degrees to radians
+        return (degree/180)*Math.PI;
+    }
+}
