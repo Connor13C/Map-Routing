8ec9f2f94954d03bcd801e3f89b6bab211ed44f9
Connor13C <connorbcarey@hotmail.com>
2018-03-02 11:53:27 -0700

    closes #205 adding naut mi

--- server/src/main/java/com/tripco/t08/planner/DistanceUnit.java
+++ server/src/main/java/com/tripco/t08/planner/DistanceUnit.java
@@ -2,7 +2,8 @@ package com.tripco.t08.planner;
 
 public enum DistanceUnit {
     KILOMETERS(6371.0088),
-    MILES(3958.7613);
+    MILES(3958.7613),
+    NAUTICALMILES(3440.0695);
 
     private final double conversionFactor;
 
@@ -18,6 +19,9 @@ public enum DistanceUnit {
     public static DistanceUnit getDistanceUnit(String dist){
         if(dist.equals("miles")){
             return DistanceUnit.MILES;
+        }
+        if(dist.equals("nautical miles")){
+            return DistanceUnit.NAUTICALMILES;
         }
         else{
             return DistanceUnit.KILOMETERS;
---
+++ server/src/main/java/com/tripco/t08/planner/Optimization.java
@@ -0,0 +1,16 @@
+package com.tripco.t08.planner;
+
+public class Optimization {
+
+    public void nearestNeighbor{
+
+    }
+
+    public void opt2{
+
+    }
+
+    public void opt3{
+
+    }
+}
