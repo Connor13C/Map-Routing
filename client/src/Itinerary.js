e2db110409e25c844d07df55f7f82b429efa3684
Connor13C <35407243+Connor13C@users.noreply.github.com>
2018-03-04 22:41:45 -0700

    Update Itinerary.js
    changed nothing other than formatting

--- client/src/Itinerary.js
+++ client/src/Itinerary.js
@@ -39,30 +39,27 @@ class Itinerary extends Component {
 
   render() {
     let table = this.createTable();
-
-    return(
-        <div id="itinerary">
-          <h4>Round trip distance of {table.distance} {table.units}. </h4>
-          <table className="table table-responsive table-bordered">
-            <thead>
-            <tr className="table-info">
-              <th className="align-middle">Destination</th>
-              {table.dests}
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <th className="table-info align-middle">{table.units}</th>
-              {table.dists}
-            </tr>
-            <tr>
-              <th className="table-info align-middle">Cumulative</th>
-                {table.cumulative}
-            </tr>
-            </tbody>
-          </table>
-        </div>
-    )
+    return(<div id="itinerary">
+             <h4>Round trip distance of {table.distance} {table.units}. </h4>
+             <table className="table table-responsive table-bordered">
+               <thead>
+                <tr className="table-info">
+                  <th className="align-middle">Destination</th>
+                  {table.dests}
+                </tr>
+               </thead>
+               <tbody>
+                <tr>
+                  <th className="table-info align-middle">{table.units}</th>
+                  {table.dists}
+                </tr>
+                <tr>
+                  <th className="table-info align-middle">Cumulative</th>
+                    {table.cumulative}
+                </tr>
+               </tbody>
+              </table>
+            </div>)
   }
 }
 
