508085cb82e5007fcbf2310884e670fb664e032d
Connor Carey <connorbcarey@hotmail.com>
2018-03-21 20:05:05 -0600

    query changed

--- server/src/main/java/com/tripco/t08/planner/Query.java
+++ server/src/main/java/com/tripco/t08/planner/Query.java
@@ -10,7 +10,6 @@ import spark.Request;
 
 import java.util.List;
 
-
 /**
  * The Query class supports TFFI so it can easily be converted to/from Json by Gson.
  *
@@ -22,42 +21,20 @@ public class Query {
     public String query;
     public List<Airport> places;
 
-    // Gson is thread safe
-    private static final Gson GSON = new GsonBuilder()
-            .registerTypeAdapter(new TypeToken<Place>(){}.getType(), new Place.Serializer())
-            .create();
-
-
-    public Query(int version, String type, String query, List<Airport> places) {
-        this.version = version;
-        this.type = type;
-        this.query = query;
-        this.places = places;
-    }
-
-    /** Handles trip planning request, creating a new trip object from the trip request.
-     * Does the conversion from Json to a Java class before planning the trip.
-     * @param request
-     */
     public Query (Request request) {
+        Gson GSON = new GsonBuilder()
+                .registerTypeAdapter(new TypeToken<Place>(){}.getType(), new Place.Serializer())
+                .create();
+
         // first print the request
         System.out.println(HTTP.echoRequest(request));
 
         // convert the body of the request to a Java class.
-        trip = GSON.fromJson(request.body(), Trip.class);
+        Query a = GSON.fromJson(request.body(), Query.class);
+        this.version=a.version;
+        this.type=a.type;
+        this.query=a.query;
+        this.places=a.places;
 
-        // plan the trip.
-        trip.plan();
-
-        // log something.
-        System.out.println(trip.title);
     }
-
-    /** Handles the response for a Trip object.
-     * Does the conversion from a Java class to a Json string.*
-     */
-    public String getQuery () {
-        return GSON.toJson(trip);
-    }
-
 }
