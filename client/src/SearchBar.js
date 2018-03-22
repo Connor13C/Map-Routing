884c8f2234af77ec8a1bcc2bb070f32e549505e3
Nate Mortensen <nate.richard.mortensen@gmail.com>
2018-03-21 22:01:06 -0600

    Finish Search bar. Closes #315

--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -11,15 +11,33 @@ export default class SearchBar extends Component {
         this.state = {
             placeName: "",
         };
+        this.onChange = this.onChange.bind(this);
         this.onNewDestinationClick = this.onNewDestinationClick.bind(this);
         this.addDestination = this.addDestination.bind(this);
         this.query = this.query.bind(this);
     }
 
+    onChange(value) {
+        let ids = new Set();
+        for (let i = 0; i < this.props.places.length; i++) {
+            ids.add(this.props.places[i].id);
+        }
+        if (ids.has(value.id)) {
+            for (let i = 0; i <= ids.size; i++) {
+                if (!ids.has(value.id+i)) {
+                    value.id+= i;
+                    break;
+                }
+            }
+        }
+        this.props.addDestination(value);
+    }
+
     onNewDestinationClick(option) {
+        console.log(option);
         // Called when they click the button to add a destination
         this.setState({
-            placeName: option.query
+            placeName: option.id
         });
     }
 
@@ -48,11 +66,12 @@ export default class SearchBar extends Component {
         return fetch('http://' + location.host + '/query', options)
             .then(response => response.json())
             .then(json => {
+                console.log(json);
                 if (json === undefined) {
                     console.log("Invalid response.");
                     return [];
                 } else {
-                    return json.places;
+                    return {options: json.places};
                 }
             }, err => {
                 console.log(err.message);
@@ -70,12 +89,19 @@ export default class SearchBar extends Component {
                                    onFinish={this.addDestination}
                                    place={{name: this.state.placeName}}/>
                 <AsyncCreatable valueKey="id"
-                                labelKey="query"
+                                labelKey="name"
                                 placeholder="Add a destination..."
+                                loadingPlaceholder="Loading..."
+                                cache={false}
                                 multi={false}
                                 loadOptions={this.query}
+                                filterOption={(option, filter) => {
+                                    // Do no filtering, just return all options
+                                    return true;
+                                }}
                                 promptTextCreator={(input) => "Create new destination " + input}
                                 onNewOptionClick={this.onNewDestinationClick}
+                                onChange={this.onChange}
                                 backSpaceRemoves />
             </div>
         );
