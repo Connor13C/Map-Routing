69f50f30a9b6ed3fdd56a14df417f7361aa084a2
[Lettia Wilson] <[lettia.wilson@gmail.com]>
2018-04-11 11:40:50 -0600

    #334 changes dropdowns to checkboxes to allow for multiple selection

--- client/src/FilteredSearch.js
+++ client/src/FilteredSearch.js
@@ -1,97 +1,149 @@
 import React, {Component} from 'react';
-import {Collapse, Button, CardBody, Card, Dropdown, DropdownMenu, DropdownToggle, DropdownItem} from 'reactstrap';
+import {
+    Collapse,
+    Button,
+    CardBody,
+    Card,
+    Dropdown,
+    DropdownMenu,
+    DropdownToggle,
+    DropdownItem,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    FormText
+} from 'reactstrap';
+import Destinations from "./Destinations";
 
 export default class FilteredSearch extends Component {
     constructor(props) {
         super(props);
         this.toggle = this.toggle.bind(this);
         this.secondToggle = this.secondToggle.bind(this);
-        this.filterData = this.filterData.bind(this);
-        this.populateDropDown2 = this.populateDropDown2.bind(this);
+        this.query = this.query.bind(this);
+        this.populateCheckBoxes = this.populateCheckBoxes.bind(this);
         this.selectValues = this.selectValues.bind(this);
         this.state = {
+            collapse: false,
             dropdownOpen: false,
             dropdown2Open: false,
-            dropdownValues: null,
+            checkBoxValues: null,
+            filter: [{
+                attribute: "",
+                values: [],
+            }],
 
         };
     }
 
 
     toggle() {
-        this.setState({
-            dropdownOpen: !this.state.dropdownOpen
-        });
+        this.setState({ collapse: !this.state.collapse });
     }
 
 
-    secondToggle(){
+    secondToggle() {
         this.setState({
             dropdown2Open: !this.state.dropdown2Open
         });
     }
 
-    populateDropDown2(e) {
+    populateCheckBoxes(e) {
         let value = e.target.value;
 
+        //console.log(this.props);
+        console.log(value);
 
-    console.log("This is the props field");
-    //console.log(this.props);
-    //console.log(value);
+        for (var i = 0; i < this.props.filters.length; i++) {
+            //console.log("Attribute: ");
+            //console.log(this.props.filters[i].attribute);
+            if (this.props.filters[i].attribute === value) {
+                {
+                    this.selectValues(i)
+                }
+                ;
+               }
 
-       for(var i = 0; i < this.props.filters.length; i++){
-           //console.log("Attribute: ");
-           //console.log(this.props.filters[i].attribute);
-           if(this.props.filters[i].attribute === value){
-               {this.selectValues(i)};
 
-           }
-       }
+        }
     }
 
-    selectValues(index){
-    let Key = "attribute" + index;
+    selectValues(index) {
+        //console.log(index);
         this.setState({
-            dropdownValues: this.props.filters[index].values.map((item, Key)=> <DropdownItem onClick={this.filterData} key={Key}> {item} </DropdownItem>)
+            checkBoxValues: this.props.filters[index].values.map((item, i) =>
+                <FormGroup check>
+                <Label check>
+                    <Input type="checkbox" onClick={(e) => this.query(e, index)} key={i} value={item}/> {item}
+                </Label>
+                </FormGroup>)
+
         });
     }
 
-    filterData(){
-        console.log("query has been selected");
+    query(e, index) {
+
+        let value = this.state.filter[0].values.push(e.target.value);
+        //this.state.filter[0].attribute += this.props.filters[index].attribute;
+        //console.log("This is value");
+        //console.log(value);
+        //console.log(this.state.filter[0].values);
+        let filters = {
+            attribute: this.props.filters[index].attribute,
+            value: this.state.filter[0].values
+        }
+        this.setState({
+                filter: filters
+            }, () => console.log(this.state.filter)
+        );
+        this.props.setFilters(this.state.filter);
+
     }
 
     render() {
-        let dropdown;
-        if(this.state.dropdownValues == null){
-            dropdown = null;
+        let checkBoxes;
+        if (this.state.checkBoxValues == null) {
+            checkBoxes= null;
         }
         else {
-            dropdown = <Dropdown isOpen={this.state.dropdown2Open} toggle={this.secondToggle}>
-                <DropdownToggle caret >
-                    Select
-                </DropdownToggle>
-                <DropdownMenu>
-                    {this.state.dropdownValues}
-                </DropdownMenu>
-            </Dropdown>;
+            checkBoxes = (<div className="card-body" style={{overflow: 'auto'}}>
+                <Form>
+                    {this.state.checkBoxValues}
+                </Form>
+                </div>);
         }
         return (
-            <div>
-
-                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                    <DropdownToggle style={{backgroundColor:"#1E4D2B"}} caret>
-                        Filters
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem onClick={this.populateDropDown2} value={"type"}>Airport type</DropdownItem>
-                        <DropdownItem onClick={this.populateDropDown2} value={"country"}>Country</DropdownItem>
-                        <DropdownItem onClick={this.populateDropDown2} value={"continent"}>Continent</DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-                {dropdown}
+            <div style={{overflow: 'auto'}}>
+                <Button color="primary" onClick={this.toggle} style={{backgroundColor: "#1E4D2B"}}>Filters</Button>
+                <Collapse isOpen={this.state.collapse}>
+                    <div id="filters" className="card">
+                        <div className="card-body">
+                            <p> Select your filter type </p>
+                            <Form>
+                                <FormGroup check inline>
+                                    <Label check>
+                                        <Input type="checkbox" value={"type"} onChange={this.populateCheckBoxes}/> Airport Type
+                                    </Label>
+                                </FormGroup>
+                                <FormGroup check inline>
+                                    <Label check>
+                                        <Input type="checkbox" value={"country"} onChange={this.populateCheckBoxes} /> Country
+                                    </Label>
+                                </FormGroup>
+                                <FormGroup check inline>
+                                    <Label check>
+                                        <Input type="checkbox" value={"continent"} onChange={this.populateCheckBoxes}/> Continent
+                                    </Label>
+                                </FormGroup>
+                            </Form>
+                            {checkBoxes}
+                        </div>
+                    </div>
+                </Collapse>
             </div>
 
-        );
+    );
     }
-}
+    }
 
