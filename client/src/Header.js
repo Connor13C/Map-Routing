63855c2d3bfd60b18bb1ce95af1be709aaa5fcad
npaul24 <35502670+npaul24@users.noreply.github.com>
2018-05-01 22:02:30 -0600

    closes #425

--- client/src/Header.js
+++ client/src/Header.js
@@ -18,16 +18,6 @@ class Header extends Component{
           <div id="header" className="jumbotron">
           {this.title()}
           <p className="lead">"Want to travel far and wide?"</p>
-            <ol><li>
-                Choose options for trip planning, information to display about locations,
-                and how the trip map and itinerary should be saved.</li>
-              <li>
-                Choose your destinations by loading existing sets of destinations or
-                find more in an extensive database of locations worldwide.</li>
-              <li>
-                Plan the trip with the options you selected.
-                Review and revise the trip origin and order.
-                Save the trip map and itinerary for future reference.</li></ol>
 <a href="About.html" className="btn btn-info" style={{backgroundColor:"#1E4D2B"}} role="button">Learn More About Us</a>
         </div>
       </div>
